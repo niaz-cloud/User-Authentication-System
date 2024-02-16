d62a01d28a6b3d9971de5b2c4b9a7c2b52552a2d
Niaz Mohammad <85242530+niaz-cloud@users.noreply.github.com>
2024-02-16 15:04:53 +0600

    Add project files.

---
+++ eTJSEstateagents/Areas/Data/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace eTJSEstateagents.Areas.Data
+{
+    public class AppUser:IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
---
+++ eTJSEstateagents/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using eTJSEstateagents.Areas.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTJSEstateagents.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
